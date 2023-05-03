28da33cbd4760b070fec0fe0f3fe7688299164db
Gh0stC0de <aaron.mueller@sunrise.ch>
2023-05-03 23:33:29 +0200

    Add sample file upload in frontend

---
+++ src/cem-cloud-frontend/src/App.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import { AuthProvider } from 'react-oidc-context';
+
+test('renders learn react link', () => {
+  const { getByText } = render(
+    <AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
+  );
+
+  expect(getByText(/Loading/i)).toBeInTheDocument();
+});
---
+++ src/cem-cloud-frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import './App.css';
+import { useAuth } from 'react-oidc-context';
+import { Counter } from './counter/Counter';
+import Demo from './demo/Demo';
+
+function App() {
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+        return <div>Anmelden...</div>;
+    case "signoutRedirect":
+        return <div>Abmelden...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Laden...</div>;
+  }
+
+  if (auth.error) {
+      return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+      return (
+      <div>
+          Hallo {auth.user?.profile.name}{" "}
+          <button onClick={() => void auth.removeUser()}>Abmelden</button>
+          <Demo />
+          <Counter />
+      </div>
+      );
+  }
+
+  return <button onClick={() => void auth.signinRedirect()}>Anmelden</button>;
+
+};
+
+export default App;
