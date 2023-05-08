ad93465f753c39f67be039765c547bf529d45c90
Gh0stC0de <aaron.mueller@sunrise.ch>
2023-05-08 18:08:32 +0200

    Add primereact UI elements to demo.

--- src/cem-cloud-frontend/src/App.tsx
+++ src/cem-cloud-frontend/src/App.tsx
@@ -1,5 +1,9 @@
+import "primereact/resources/primereact.min.css";
+import 'primeicons/primeicons.css';
+import "./theme.css";
 import './App.css';
 import { useAuth } from 'react-oidc-context';
+import { Button } from 'primereact/button'
 import { Counter } from './counter/Counter';
 import Demo from './demo/Demo';
 
@@ -25,14 +29,14 @@ function App() {
       return (
       <div>
           Hallo {auth.user?.profile.name}{" "}
-          <button onClick={() => void auth.removeUser()}>Abmelden</button>
+          <Button onClick={() => void auth.removeUser()}>Abmelden</Button>
           <Demo />
           <Counter />
       </div>
       );
   }
 
-  return <button onClick={() => void auth.signinRedirect()}>Anmelden</button>;
+  return <Button onClick={() => void auth.signinRedirect()}>Anmelden</Button>;
 
 };
 
