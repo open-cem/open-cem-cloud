5a8c2651c3252481edb429667954f707217bb84b
Nils Stutz <nils.stutz@students.fhnw.ch>
2023-05-22 08:35:32 +0200

    Fix test and documentation

--- src/cem-cloud-backend/src/test/java/ch/fhnw/cemcloudbackend/controller/ConfigurationControllerTest.java
+++ src/cem-cloud-backend/src/test/java/ch/fhnw/cemcloudbackend/controller/ConfigurationControllerTest.java
@@ -17,7 +17,7 @@ class ConfigurationControllerTest {
 
         // Call getConfiguration() method
         ConfigurationController controller = new ConfigurationController();
-        String actual = controller.getConfiguration();
+        String actual = controller.getConfiguration("123", "abc");
 
         // Compare expected and actual
         assertEquals(ignoreDynamicFields(expected), ignoreDynamicFields(actual));
