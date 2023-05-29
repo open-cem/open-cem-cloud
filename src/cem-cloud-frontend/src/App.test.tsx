f32ef91abd13d71a0e75bac09c674d21677a7277
Gh0stC0de <aaron.mueller@sunrise.ch>
2023-05-29 14:47:37 +0200

    Fix default app loading test

--- src/cem-cloud-frontend/src/App.test.tsx
+++ src/cem-cloud-frontend/src/App.test.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import { AuthProvider } from 'react-oidc-context';
 
-test('renders learn react link', () => {
+test('renders loading on startup', () => {
   const { getByText } = render(
     <AuthProvider>
       <Provider store={store}>
@@ -14,5 +13,5 @@ test('renders learn react link', () => {
     </AuthProvider>
   );
 
-  expect(getByText(/Loading/i)).toBeInTheDocument();
+  expect(getByText(/Laden/i)).toBeInTheDocument();
 });
