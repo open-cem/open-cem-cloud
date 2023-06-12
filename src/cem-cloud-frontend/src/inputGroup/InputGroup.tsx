b784f39e0da9e7e1c201da409a77f86d1f625c6e
Gh0stC0de <aaron.mueller@sunrise.ch>
2023-06-12 21:18:48 +0200

    Add types for InputGroup parameters

--- src/cem-cloud-frontend/src/inputGroup/InputGroup.tsx
+++ src/cem-cloud-frontend/src/inputGroup/InputGroup.tsx
@@ -1,21 +1,36 @@
 import { InputText } from "primereact/inputtext";
 import { Image } from "primereact/image";
 import "./InputGroup.css";
+import { ChangeEventHandler } from "react";
 
-export const InputGroup = (props: any) => {
+/**
+ * Shows a label and an input field.
+ * 
+ * @param label The label of the input field
+ * @param id The id which connects the label and the input field (for - id)
+ * @param changeFn The onChange function of the input field
+ * @param name The name of the input field
+ * @param value The value of the input field
+ * @param isReadOnly True, if the input field is read only, else false
+ */
+export const InputGroup = ({ label, id, changeFn, name, value, isReadOnly }: { label: string, id: string, changeFn: ChangeEventHandler<HTMLInputElement>, name: string, value: string | undefined, isReadOnly?: boolean | undefined | null }) => {
+    if (!isReadOnly) {
+        isReadOnly = false;
+    }
+
     return (
         <div className="input-group">
-            <label htmlFor={props.id}>{props.label}</label>
-            <InputText id={props.id} value={props.value} onChange={(e) => props.setValue(e.target.value)} />
+            <label htmlFor={id}>{label}</label>
+            <InputText id={id} value={value} name={name} onChange={changeFn} readOnly={isReadOnly} />
         </div>
     )
 };
 
-export const PictureInputGroup = (props: any) => {
+export const PictureInputGroup = ({ id, label }: { id: string, label: string }) => {
     return (
         <div className="input-group">
-            <label htmlFor={props.id}>{props.label}</label>
-            <Image id={props.id} src="/img/placeholder.png" alt="Image" />
+            <label htmlFor={id}>{label}</label>
+            <Image id={id} src="/img/placeholder.png" alt="Image" />
         </div>
     )
 };
