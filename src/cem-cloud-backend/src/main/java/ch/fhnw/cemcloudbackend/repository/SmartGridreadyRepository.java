9ab83497d094489f54641fbf0ea2223798a57070
Nils Stutz <nils.stutz@students.fhnw.ch>
2023-07-03 11:46:14 +0200

    Renamed file

--- src/cem-cloud-backend/src/main/java/ch/fhnw/cemcloudbackend/repository/SmartGridreadyRepository.java
+++ src/cem-cloud-backend/src/main/java/ch/fhnw/cemcloudbackend/repository/SmartGridreadyRepository.java
@@ -1,32 +1,9 @@
 package ch.fhnw.cemcloudbackend.repository;
 
-import ch.fhnw.cemcloudbackend.configuration.ApplicationProperties;
+import ch.fhnw.cemcloudbackend.entity.SmartGridreadyDefinition;
+import org.springframework.data.repository.CrudRepository;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.UUID;
 
-public class SmartGridReadyRepository {
-    private final Path uploadDirPath;
-
-    public SmartGridReadyRepository(ApplicationProperties applicationProperties) {
-        uploadDirPath = Paths.get(applicationProperties.uploadDirectory()).toAbsolutePath().normalize().resolve("smartgridready");
-    }
-
-    public Optional<byte[]> load(UUID id) throws IOException {
-        if (id == null) {
-            throw new IllegalArgumentException("id can not be null");
-        }
-
-        String todoFilename = "SGr_HeatPump_Test.xml";
-        Path xmlFile = uploadDirPath.resolve(todoFilename);
-        if (Files.exists(xmlFile)) {
-            return Optional.of(Files.readAllBytes(xmlFile));
-        }
-
-        return Optional.empty();
-    }
+public interface SmartGridreadyRepository extends CrudRepository<SmartGridreadyDefinition, UUID> {
 }
