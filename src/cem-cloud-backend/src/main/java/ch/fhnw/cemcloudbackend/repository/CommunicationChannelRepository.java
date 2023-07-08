679228347af156a7ee68a7b9b06506fc0e16f7fc
Gh0stC0de <aaron.mueller@sunrise.ch>
2023-07-08 11:16:51 +0200

    Add access control to communication channels controler

--- src/cem-cloud-backend/src/main/java/ch/fhnw/cemcloudbackend/repository/CommunicationChannelRepository.java
+++ src/cem-cloud-backend/src/main/java/ch/fhnw/cemcloudbackend/repository/CommunicationChannelRepository.java
@@ -1,10 +1,19 @@
 package ch.fhnw.cemcloudbackend.repository;
 
 import ch.fhnw.cemcloudbackend.entity.CommunicationChannel;
+import ch.fhnw.cemcloudbackend.model.Role;
+import ch.fhnw.cemcloudbackend.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CommunicationChannelRepository extends CrudRepository<CommunicationChannel, UUID> {
-    Iterable<CommunicationChannel> findAllByInstallationId(UUID installationId);
+    Optional<CommunicationChannel> findByInstallationInstallationAccessesUserIdAndId(UUID userId, UUID id);
+
+    default Optional<CommunicationChannel> getChannel(UUID id, User user) {
+        return user.isInAnyRole(Role.ADMINISTRATOR, Role.INSTALLATEUR)
+                ? findById(id)
+                : findByInstallationInstallationAccessesUserIdAndId(user.getId(), id);
+    }
 }
