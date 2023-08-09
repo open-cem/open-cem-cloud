7537e1e05dc3d5d1f376e9e1c511b83826b6541b
Gh0stC0de <Gh0stC0de@users.noreply.github.com>
2023-08-09 21:18:13 +0200

    Reflect isLogging switch changes to the UI

--- src/cem-cloud-frontend/src/component/Component.tsx
+++ src/cem-cloud-frontend/src/component/Component.tsx
@@ -30,7 +30,7 @@ const Component = () => {
     const [isSmartGridready, setIsSmartGridready] = useState<boolean>(false);
     const [smartGridReadyFiles, setSmartGridReadyFiles] = useState<SmartGridreadyFile[]>([]);
     const [selectedSmartGridreadyFile, setSelectedSmartGridreadyFile] = useState<SmartGridreadyFile | undefined>(undefined);
-    const [isLogging, setIstLogging] = useState<boolean | undefined>(false);
+    const [isLogging, setIsLogging] = useState<boolean | undefined>(false);
 
     const installationId: string = params.installationId ?? "";
 
@@ -79,7 +79,7 @@ const Component = () => {
                     setSelectedChannel(_.find(cs, x => x.id === c.channelId));
                     setIsSmartGridready(c.smartGridreadyDefinitionId !== null);
                     setSelectedSmartGridreadyFile(_.find(fs, f => f.id === c.smartGridreadyDefinitionId));
-                    setIstLogging(c.isLogging);
+                    setIsLogging(c.isLogging);
                 }
             })
             .catch(e => presentError('Komponente konnnte nicht geladen werden, versuchen Sie es später erneut.', undefined, e, toast.current));
@@ -160,6 +160,11 @@ const Component = () => {
         setSelectedModel(undefined);
     };
 
+    const onIsLoggingChanged = (isLogging: boolean) => {
+        setIsLogging(isLogging);
+        onChange([{propertyName: 'isLogging', value: isLogging}], component);
+    };
+
     const onSave = () => {
         if (auth.user && !(component instanceof NullComponent)) {
             new ComponentsService()
@@ -195,7 +200,7 @@ const Component = () => {
                                 </>
                             }
                             <DropdownInputGroup id="formChannel" label="Kommunikationskanal" value={selectedChannel} options={channels} optionLabel="name" onChangeFn={(e) => onSelectedChannelChanged(e.target.value, component)} />
-                            <SwitchInputGroup id="formIsLogging" label="Loggen?" value={isLogging} onChangeFn={(e) => onChange([{propertyName: 'isLogging', value: e.target.value ?? false}], component)} />
+                            <SwitchInputGroup id="formIsLogging" label="Loggen?" value={isLogging} onChangeFn={(e) => onIsLoggingChanged(e.target.value ?? false)} />
                         </>
                         : <></>
                 }
